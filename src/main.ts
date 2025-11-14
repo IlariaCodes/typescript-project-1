ce2fee45d14d9c3dfb325ece5e7683dc4ba9ae30
Ilaria <ilaria.dimarco@itsincom.it>
2025-11-14 09:54:35 +0100

    prova commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-/* eslint-disable no-console */
+const number1 = Number(prompt("Inserisci il voto numero 1: "));
+const number2 = Number(prompt("Inserisci il voto numero 2: "));
+const number3 = Number(prompt("Inserisci il voto numero 3: "));
+const number4 = Number(prompt("Inserisci il voto numero 4: "));
+const number5 = Number(prompt("Inserisci il voto numero 5: "));
 
-function main(): void
+const media = Number(number1 + number2 + number3 + number4 + number5) / 5;
+
+if (
+    (number1 > 0 || number1 <= 10) &&
+    (number2 > 0 || number2 <= 10) &&
+    (number3 > 0 || number3 <= 10) &&
+    (number4 > 0 || number4 <= 10) &&
+    (number5 > 0 || number5 <= 10)
+)
 {
-    console.log("Hello, world!");
+    alert("La tua media è di: " + media);
 }
 
-main();
